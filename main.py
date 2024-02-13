c74c6aa51fd89ed7a8dc12a0b50c0ae965543dfa
OMalenivskyi <s01081995@gmail.com>
2024-02-13 23:14:23 +0200

    Task2

--- main.py
+++ main.py
@@ -1,37 +1,3 @@
-# # print("Hello player!")
-# #
-# # first_diagonal = int(input("Enter the first diagonal: "))
-# # second_diagonal = int(input("Enter the second diagonal: "))
-# # area = (first_diagonal * second_diagonal) / 2
-# # print("Total: ", area)
-# #
-# # v1
-# # n1 = 10
-# # n2 = 20
-# # v2
-# n1, n2 = 10, 20 # множинне привласнення
-# print(n1 > n2)
-# print(n1 >= n2)
-# print(n1 < n2)
-# print(n1 <= n2)
-# print(n1 == n2) # поверне true якщо обидва операнди однакові
-# print(n1 != n2) # поверне true якщо обидва операнди різні
-#
-# # print(1 == 1 and 3 = 3) # поверне true якщо обидва операнди рівні true, інашке false
-# # print(1 == 1 or 2 = 3) # поверне true якщо хоча б один операнд дорівнює true, інашке false
-#
-# is_valid = False
-# print(is_valid)
-# print(not is_valid) # not --> інверсіяб якщо значення False стане True, і навпаки
-#
-# print("hello" in "Hello world")
-#
-#
-hours = int(input("Enter hours: "))
+2. Користувач вводить з клавіатури кількість метрів.
+Залежно від вибору користувача програма переводить метри милі, дюйми або ярди.
 
-if hours >= 12 and hours <= 23:
-    print("PM")
-elif hours >= 0 and hours < 12:
-    print("AM")
-else:
-    print("Incorrect hours!!!")
