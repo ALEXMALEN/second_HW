73926d4d454d401dc0174d70e8efbb35576ffcb7
OMalenivskyi <s01081995@gmail.com>
2024-02-14 00:03:46 +0200

    Task 2 completed!

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-2. Користувач вводить з клавіатури кількість метрів.
-Залежно від вибору користувача програма переводить метри милі, дюйми або ярди.
+meters = float(input("Enter meter quantity: "))
 
+choice = input("Choose an operation (miles, inches or yards): ")
+
+if choice == "miles":
+    result = meters * 0.000621371
+    print("Quantity miles: ", result)
+elif choice == "inches":
+    result = meters * 39.3701
+    print("Quantity inches: ", result)
+elif choice == "yards":
+    result = meters * 1.09361
+    print("Quantity yards: ", result)
+else:
+    print("Incorrect operation selection. Please select 'miles', 'inches' or 'yards'.")
